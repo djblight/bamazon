e14c9a4a2f385966ab188acd9a00f22152e1e127
djblight <djblight@bellsouth.net>
2017-08-28 20:07:45 -0400

    final commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,85 +3,89 @@ var inquirer = require("inquirer");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+    host: "localhost",
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "1@Dra2@Aus3",
-  database: "bamazon_db"
+    // Your password
+    password: "1@Dra2@Aus3",
+    database: "bamazon_db"
 });
 
 // connect to the mysql server and sql database
 connection.connect(function(err) {
-  if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  // start();
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    // start();
 });
 
 // Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-function itemPurchase() {
-    // query the database for all items listed for sale
-    connection.query("SELECT * FROM products", function(err, results) {
-      if (err) throw err;
-      // once you have the items, prompt the user for which they'd like to purchase
-      // inquirer
-      inquirer
-      .prompt([
+function start() {
+
+    //Query products, id, and prices to display to customer
+    connection.query("SELECT item_id, product_name, price FROM products", function(error, data) {
+        if (error) {
+            return console.log("Error!");
+        }
+        for (var i = 0; i < data.length; i++) {
+            var list = "ID. #" + data[i].item_id + " || Item: " + data[i].product_name + ", $" + data[i].price;
+            console.log(list);
+        }
+    });
+}
+
+function service() {
+    inquirer.prompt([
         {
-          name: "productId",
-          type: "input",
-          message: "What is the ID of the item you would like to purchase?"
+            name: "id",
+            message: "What is the ID of the item you want to buy?",
+            type: "input"
         },
         {
-          name: "buyUnits",
-          type: "input",
-          message: "How many units would you like to purchase?",
-          validate: function(value) {
-            if (isNaN(value) === false) {
-              return true;
-            }
-              return false;
-            }
-          }  
-      ])
-      .then(function(answer) {
-        // get the information of the chosen item
-        var chosenItem;
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].item_name === answer.choice) {
-            chosenItem = results[i];
-          }
+            name: "desiredQuantity",
+            message: "How many units would you like to buy of this item?",
+            type: "input"
+        }
+    ]).then(function(response) {
+        var id = response.id;
+        var desiredQuantity = response.desiredQuantity;
+
+        //Check if there is enough in stock of the item using parameters
+        check(id, desiredQuantity);
+    });
+}
+
+function check(id, desiredQuantity) {
+    connection.query("SELECT stock_quantity, price FROM products WHERE ?", {
+        item_id: id
+    }, function(error, data) {
+        if (error) {
+            return console.log("Error!");
         }
 
-        // determine if stock quantity available is high enough
-        if (chosenItem.stock_quantity < parseInt(answer.buyUnits)) {
-          // if stock quantity is high enough, update db, let the user know, and show total coat of order
-          connection.query(
-            "UPDATE products SET ? WHERE ?",
-            [
-              {
-                stock_quantity: answer.buyUnits
-              },
-              {
-                id: chosenItem.productId
-              }
-            ],
-            function(error) {
-              if (error) throw err;
-              var total = (answer.quantity * chosenItem.price);
-              console.log("Order placed successfully! Your order total is " + total);
-              start();
-            }
-          );
+        var inStock = parseInt(data[0].stock_quantity);
+        var price = parseFloat(data[0].price);
+
+        if (desiredQuantity <= inStock) {
+            var totalPrice = price * desiredQuantity;
+            var inStock = inStock - desiredQuantity;
+
+            connection.query("UPDATE products SET ? WHERE ?", [{
+                stock_quantity: inStock
+            }, {
+                item_id: id
+            }], function(error, data) {
+                if (error) {
+                    return console.log("Error!");
+                }
+            });
+
+            return console.log("Purchase successful! Your total price is: $" + totalPrice.toFixed(2));
+
+        } else {
+            return console.log("Insufficient quantity!");
         }
-        else {
-          // stock quantity wasn't high enough, so apologize and start over
-          console.log("Your desired item is not instock, but you can order a different item...");
-          start();
-        }
-      })
-    })
+    });
 }
